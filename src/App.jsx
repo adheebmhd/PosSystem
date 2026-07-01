c37f94363536d27937ae0b80d0fb16bdde491bb7
adheebmhd <adheebmhd7@gmail.com>
2026-07-01 23:13:50 +0530

    logic case

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,36 @@
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom"
-import{Login,Menu,Bills,Payment,Report,Stock,User} from "./pages";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { Login, Menu, Bills, Payment, Report, Stock, User } from "./pages";
 import { Header } from "./components/shared";
 
+function AppWrapper() {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+}
 
 function App() {
-  const hideHeader = location.pathname === "/" ;
-  const hideHeade = location.pathname ===  "/user";
+  const location = useLocation();
 
-  
+  const hideHeader =
+    location.pathname === "/" || location.pathname === "/user";
 
   return (
     <>
-      <Router>
-        {!hideHeader && !hideHeade && <Header />}
-        
-        <Routes>
-          <Route path="/" element={<Login />}  />
-          <Route path="/meanu" element={<Menu />} />
-          <Route path="/bills" element={<Bills />} />
-          <Route path="/payment" element={<Payment />}/>
-          <Route path="/report" element={<Report />}/>
-          <Route path="/stock" element={<Stock />}/>
-          <Route path="/user" element={<User />}/>
-        </Routes>
-      </Router>
-      
+      {!hideHeader && <Header />}
+
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/meanu" element={<Menu />} />
+        <Route path="/bills" element={<Bills />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/report" element={<Report />} />
+        <Route path="/stock" element={<Stock />} />
+        <Route path="/user" element={<User />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default AppWrapper;
