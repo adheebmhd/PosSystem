9dfd0ff7c65dbf0e9068edf3b5d335847be2b536
adheebmhd <adheebmhd7@gmail.com>
2026-07-03 21:08:01 +0530

    12

--- src/pages/biils.jsx
+++ src/pages/biils.jsx
@@ -360,7 +360,10 @@ const deleteDraft = (id) => {
             </button>
 
             <button
-              onClick={() => deleteDraft(d.id)}
+              onClick={async () => {
+                await handlePrint();
+                await deleteDraft(d.id);
+              }}
               className="bg-red-600 text-white px-2 py-1 rounded"
             >
               Delete
